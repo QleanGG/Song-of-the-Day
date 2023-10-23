526ad2146e43e2221d22afbe12eb3e4778d7db89
QleanGG <guyguz1@gmail.com>
2023-10-23 17:35:07 +0300

    add js program

---
+++ main.js
@@ -0,0 +1,13 @@
+/*This is going to be a project suggesting the artist of the day, 
+Featured album and featured song
+
+This can be made using an objet (the name of the artist) 
+inside of it having an array of albums 
+inside of it having an array of song
+*/
+
+// creating an artist
+
+function artistFactory() {
+    
+}
