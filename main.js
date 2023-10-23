5214bae1b1ffff454ed33ff4095f60a6f7b0417b
QleanGG <guyguz1@gmail.com>
2023-10-23 22:59:45 +0300

    add function to declare artist object and dpr ian

--- main.js
+++ main.js
@@ -6,8 +6,48 @@ inside of it having an array of albums
 inside of it having an array of song
 */
 
-// creating an artist
-
-function artistFactory() {
-    
+// creating album Factory function
+// albumArr should include in every index an array where the first index is the name and second index as array of songlist
+const albumFactory = (albumArr) => {
+    const albumOrder = []
+    for (i = 0; i < albumArr.length; i++){
+        albumOrder.push({
+            _albumName: albumArr[i][0],
+            _songList: albumArr[i][1],
+            get albumName() {
+                return this._albumName;
+            },
+            get randomSong() {
+                // const songCount = this._songList
+                return this._songList[Math.floor(Math.random() * this._songList.length)];
+            },
+        })
+    }
+    return albumOrder;
 }
+
+
+// creating artist Factory function
+
+const artistFactory = (name, albumArr ) => {
+    return {
+        artist: name,
+        albums: albumFactory(albumArr),
+    };
+}
+
+/*
+DPR Ian
+['moodswings in this order', 'moodswings in to order', 'dear insanity...']
+loop in loop
+album will be an array with first index as the name (string) and song list as array for 2nd index
+*/
+
+const dprIan = artistFactory('DPR Ian',[
+    ['Moodswings In This Order',['MITO','So Beautiful','Dope Lovers','No Blueberries','Nerves','Scaredy Cat','Welcome To The Show','No Silhouette']],
+    ['Moodswing In To Order',['Seraph','1 Shot','Mood','Miss Understood','Avalon','Merry Go','Ribbon','Winterfell','Calico','Mr. Insanity','Ballroom Extravaganza','Sometimes I\'m']],
+    ['Dear Insanity...',['Famous Last Words','Welcome To The Other Side',"Don't Go Insane",'Bad Cold','So I Danced','Peanut Butter & Tears', 'Violet Crazy']]]);
+// console.log (dprIan.artist);
+// console.log (dprIan.albums[2].albumName);
+// console.log (dprIan.albums[2].randomSong);
+// hello
