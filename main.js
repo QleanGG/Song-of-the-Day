4a5bdd05125708a17d2f2b67ee3b550c640d4a48
QleanGG <guyguz1@gmail.com>
2023-12-03 14:45:17 +0200

    added youtube api integration to show the song video on the website

--- main.js
+++ main.js
@@ -8,6 +8,31 @@ inside of it having an array of songs
 
 import artists from "./modules/artists.js";
 
+const YOUTUBE_API_KEY = 'AIzaSyAU8L3Ul7gYe3UIByqf5Nbi4XLQQVcpTHk'; // Replace with your YouTube API key
+
+const searchYouTube = async (artistName, songName) => {
+    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(`${artistName} ${songName}`)}&type=video&key=${YOUTUBE_API_KEY}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.items && data.items.length > 0) {
+            const videoId = data.items[0].id.videoId;
+            const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+            console.log(`Watch on YouTube: ${videoUrl}`);
+            const videoContainer = document.getElementById('youtube-video');
+            videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+        } else {
+            console.error('No search results found on YouTube.');
+        }
+    } catch (error) {
+        console.error('Error fetching data from YouTube API:', error);
+    }
+};
+
+
+
 //Function to announce the random artist, random album and random song
 
 function randomArtist() {
@@ -17,12 +42,16 @@ function randomArtist() {
     
 }
 
-const randomMsg = artist => {
+const randomMsg = async artist => {
     console.log(`Artist of the day is "${artist.name}"`);
     const randAlbumNum = Math.floor(Math.random() * artist.albums.length);
     const randAlbum = artist.albums[randAlbumNum];
+    const randomSong = randAlbum.randomSong();
     console.log(`Featured Album is "${randAlbum.albumName}"`);
-    console.log(`Featured Song is "${randAlbum.randomSong()}"`);
+    console.log(`Featured Song is "${randomSong}"`);
+
+    await searchYouTube(artist.name, randomSong);
+
 
     // putting it on the html
     const artistContainer = document.getElementById('random-artist');
@@ -30,14 +59,14 @@ const randomMsg = artist => {
     <h2>Artist of the day is:"${artist.name}"</span><br>
     <h3>Album of the day is:"${randAlbum.albumName}"</h3><br>
     <img src = "./${randAlbum.image}" width=400px height=400px><br>
-    <p>Featured Song is:"${randAlbum.randomSong()}"</p>`;
+    <p>Featured Song is:"${randomSong}"</p>`;
 }
 
 //Function that take an array of artist objects and randomly picks an artist
-const allArtistsRandomMessage = artistsArr => randomMsg(artistsArr[Math.floor(Math.random() * artistsArr.length)]);
+// const allArtistsRandomMessage = artistsArr => randomMsg(artistsArr[Math.floor(Math.random() * artistsArr.length)]);
 
 
-const allArtists = Object.values(artists);
+// const allArtists = Object.values(artists);
 
 
 randomMsg(randomArtist())
